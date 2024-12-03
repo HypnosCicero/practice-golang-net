1b01d4d7a724418b5dae7c636c7a0f8733b1871f
Hypnosis-Cicero <1345764659@qq.com>
2024-12-03 22:59:57 +0800

    feat: init go file in ch3 folder

---
+++ ip.go
@@ -0,0 +1,23 @@
+/* IP
+ */
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage: %s ip-addr\n", os.Args[0])
+	}
+	name := os.Args[1]
+	addr := net.ParseIP(name)
+	if addr == nil {
+		fmt.Println("Invalid address")
+	} else {
+		fmt.Println("The address is", addr.String())
+	}
+}
