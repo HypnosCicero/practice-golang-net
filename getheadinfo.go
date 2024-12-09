bd00dfd1d32bb542a31ced37832983fbb5845acb
Hypnosis-Cicero <1345764659@qq.com>
2024-12-09 21:01:44 +0800

    feat(get-head-info): add the `getheadinfo.go`

---
+++ getheadinfo.go
@@ -0,0 +1,32 @@
+/* GetHeadInfo
+ */
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage: %s host:port", os.Args[0])
+	}
+	service := os.Args[1]
+	tcpAdrr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+	conn, err := net.DialTCP("tcp", nil, tcpAdrr)
+	checkError(err)
+	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	checkError(err)
+	result, err := ioutil.ReadAll(conn)
+	fmt.Println(string(result))
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln("get head info Fatal error: %s", err.Error())
+	}
+}
