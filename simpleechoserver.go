e40319c0f06a3ce798ca1d59c011b43d1c85e83d
Hypnosis-Cicero <1345764659@qq.com>
2024-12-13 08:31:19 +0800

    feat: add the `simpleechoserver.go`

---
+++ simpleechoserver.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	service := ":1201"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			continue
+		}
+		handleClient(conn)
+		conn.Close()
+	}
+}
+
+func handleClient(conn net.Conn) {
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		checkError(err)
+		fmt.Println(string(buf[0:]))
+		_, err = conn.Write(buf[0:n])
+		checkError(err)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln("Simple echo server Fatal error: %s", err.Error())
+	}
+}
