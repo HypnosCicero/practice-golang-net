1c23e772a51704a76fe78790dab978739606d0af
Hypnosis-Cicero <1345764659@qq.com>
2024-12-13 22:50:28 +0800

    feat(`threadedechoserver`): add the `threadedechoserver`

---
+++ threadedechoserver.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	service := ":1201"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	// close connection on exit
+	defer conn.Close()
+	var buf [512]byte
+	for {
+		// read up to 512 bytes
+		n, err := conn.Read(buf[0:])
+		checkError(err)
+		fmt.Println(string(buf[0:]))
+		// write the n bytes read
+		_, err = conn.Write(buf[0:n])
+		checkError(err)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln("Threaded echo server Fatal error %s", err.Error())
+	}
+}
